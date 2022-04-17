52fa7afb95c23dd9e1bdd3d8fc8b7fcdc85c3e32
kangkyu <kangkyu1111@gmail.com>
2022-04-17 03:59:33 -0700

    Add sarama and envdecode

---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/Shopify/sarama"
+	"github.com/joeshaw/envdecode"
+)
+
+type AppConfig struct {
+	Kafka struct {
+		URL           string `env:"KAFKA_URL,required"`
+		// TrustedCert   string `env:"KAFKA_TRUSTED_CERT,required"`
+		// ClientCertKey string `env:"KAFKA_CLIENT_CERT_KEY,required"`
+		// ClientCert    string `env:"KAFKA_CLIENT_CERT,required"`
+		// Prefix        string `env:"KAFKA_PREFIX"`
+		// Topic         string `env:"KAFKA_TOPIC,default=poems"`
+		// ConsumerGroup string `env:"KAFKA_CONSUMER_GROUP,default=poems-go"`
+	}
+}
+
+func main() {
+	appconfig := AppConfig{}
+	envdecode.MustDecode(&appconfig)
+
+	config := sarama.NewConfig()
+	config.ClientID = "go-kafka-consumer"
+	config.Consumer.Return.Errors = true
+
+	brokers := []string{appconfig.Kafka.URL}
+
+	// Create new consumer
+	master, err := sarama.NewConsumer(brokers, config)
+	if err != nil {
+		fmt.Println("Hello here's an error")
+		panic(err)
+	}
+
+
+	defer func() {
+		if err := master.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	topics, _ := master.Topics()
+
+	consumer, errors := consume(topics, master)
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	// Count how many message processed
+	msgCount := 0
+
+	// Get signnal for finish
+	doneCh := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case msg := <-consumer:
+				msgCount++
+				fmt.Println("Received messages", string(msg.Key), string(msg.Value))
+			case consumerError := <-errors:
+				msgCount++
+				fmt.Println("Received consumerError ", string(consumerError.Topic), string(consumerError.Partition), consumerError.Err)
+				doneCh <- struct{}{}
+			case <-signals:
+				fmt.Println("Interrupt is detected")
+				doneCh <- struct{}{}
+			}
+		}
+	}()
+
+	<-doneCh
+	fmt.Println("Processed", msgCount, "messages")
+
+}
+
+func consume(topics []string, master sarama.Consumer) (chan *sarama.ConsumerMessage, chan *sarama.ConsumerError) {
+	consumers := make(chan *sarama.ConsumerMessage)
+	errors := make(chan *sarama.ConsumerError)
+	for _, topic := range topics {
+		if strings.Contains(topic, "__consumer_offsets") {
+			continue
+		}
+		partitions, _ := master.Partitions(topic)
+		// this only consumes partition no 1, you would probably want to consume all partitions
+		consumer, err := master.ConsumePartition(topic, partitions[0], sarama.OffsetOldest)
+		if nil != err {
+			fmt.Printf("Topic %v Partitions: %v", topic, partitions)
+			panic(err)
+		}
+		fmt.Println(" Start consuming topic ", topic)
+		go func(topic string, consumer sarama.PartitionConsumer) {
+			for {
+				select {
+				case consumerError := <-consumer.Errors():
+					errors <- consumerError
+					fmt.Println("consumerError: ", consumerError.Err)
+
+				case msg := <-consumer.Messages():
+					consumers <- msg
+					fmt.Println("Got message on topic ", topic, msg.Value)
+				}
+			}
+		}(topic, consumer)
+	}
+
+	return consumers, errors
+}
